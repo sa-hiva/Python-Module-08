3eec1dc5158a47a65049ff827547a508c56e7004
Sara <sarahierrovazquez@gmail.com>
2026-07-17 16:43:39 +0200

    module 08 ex 1 aa meddias

--- ex1/loading.py
+++ ex1/loading.py
@@ -1,8 +1,9 @@
-import importlib, importlib_metadata
+import importlib
+from importlib import metadata
 
-DEPENDENCIES = [
-    ("pandas", "Data manipulation ready")
-    ("numpy", "Numerical computation ready")
+DEPENDENCIES: list[tuple[str, str]] = [
+    ("pandas", "Data manipulation ready"),
+    ("numpy", "Numerical computation ready"),
     ("matplotlib", "Visualization ready")
 ]
 
@@ -14,7 +15,7 @@ def valid_dependencies() -> bool:
         ready = dependency[1]
         try:
             importlib.import_module(name)
-            version = importlib.metadata.version(name)
+            version = metadata.version(name)
             print(f"[OK] {name} {version} - {ready}")
         except ImportError:
             print(f"[MISSING] {name} - Not installed")
@@ -27,6 +28,22 @@ def print_instructions() -> None:
     print("pip install -r requirements.txt")
     print("To install it whith Poetry:")
     print("  poetry install")
+
+
+# Will simulate a program that analyzes metheorological data
+
+def analyze_data() -> None:
+    import pandas as pd
+    import numpy as np
+    dates = pd.date_range("2000-01-01", periods=365)
+    days = np.arrange(365 * 25)
+    
+    base_temperature = 15
+    season = np.sin((2 * np.pi * days / 365) - np.pi / 2)
+    season_amplitude = 10
+    noise = np.random.normal(0, 2, size=365)
+    trend = np.linspace(0, 1.5, 365)
+    temperature = base_temperature + season_amplitude * season + noise
 
 
 if __name__ == "__main__":
